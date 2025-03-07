709f84db16ab31a7a87acf3983a73a43f313ee68
Ryan <164410629+Rdtcm@users.noreply.github.com>
2025-03-07 04:37:07 +0000

    coloquei o cabecalho de print no menu e troquei os ifs pelo switch case

--- menu.java
+++ menu.java
@@ -3,30 +3,50 @@ import java.util.Scanner;
 
 public class menu {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int op;
         String expressaoNumerica;
         //fazendo o menu
         while(true) {
+            System.out.println("\n");
+            System.out.println("\033[1;34m*************************************\033[0m");
+            System.out.println("\033[1;34m*               Menu                *\033[0m");
+            System.out.println("\033[1;34m*************************************\033[0m");
+            System.out.println("\033[1;32m1. Opção 1: Entrada da expressão aritmética na notação infixa X\033[0m");
+            System.out.println("\033[1;32m2. Opção 2: Entrada dos valores numéricos associados às variáveis Y\033[0m");
+            System.out.println("\033[1;32m2. Opção 3: Converter Expressao e Mostrar Y\033[0m");
+            System.out.println("\033[1;32m2. Opção 4: Apresentar Resultados Y\033[0m");
+            System.out.println("\033[1;32m3. Opção 5: Sair\033[0m");
+            System.out.println("\033[1;34m*************************************\033[0m");
+
             System.out.println("Digite a opção desejada: ");
-            op = scan.nextInt();
+            op = scanner.nextInt();
 
-            if (op == 1) {
-                System.out.println("Digite a expressão: ");
-                expressaoNumerica = scan.nextLine();
-            }
-            if (op == 2) {
+            scanner.nextLine();
 
-            }
-            if (op == 3) {
+            switch (op) {
+                case 1:
+                    System.out.println("Digite a expressão: ");
+                    expressaoNumerica = scanner.nextLine();
+                    System.out.println("Voce digitou a expressao: " + expressaoNumerica);
+                    break;
 
-            }
-            if (op == 4) {
+                case 2:
 
-            }
-            if (op == 5) {
-                System.out.println("");
-                break;
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+                    System.out.println("Programa encerrado");
+                    scanner.close();
+                    break;
             }
         }
     }
